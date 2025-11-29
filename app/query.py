c8fddb5e120154cea04a53d644ffe00fcecec67a
Spencer Brian Baiden <brianbaiden@gmail.com>
2025-11-29 22:31:10 +0100

    Update LLM_BASE_URL to new host

--- app/query.py
+++ app/query.py
@@ -17,7 +17,7 @@ class Config:
     EMBEDDING_MODEL = "nomic-embed-text"
     # On laisse le choix du modèle LLM dans l'interface
     DEFAULT_LLM_MODEL = "mistral:7b-instruct-q5_K_M"
-    LLM_BASE_URL = os.getenv("LLM_BASE_URL", "http://localhost:11434")
+    LLM_BASE_URL = os.getenv("LLM_BASE_URL", "http://stupefied_curie:11434")
 
 PROMPT_TEMPLATE = """Tu es un assistant francophone strictement ancré au contexte fourni.
 Réponds en français, de façon concise et exacte.
